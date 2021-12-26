1311be2fa1594e63a46acaf7079f106b47c9ba20
Max <maximrimkus@yandex.by>
2021-12-26 19:02:48 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSUIR_OOTPSISP_LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
